fc96403381c7682a2dd6d58b0e7c91344513fe41
Jocelyn Geiger <geigerjm312@gmail.com>
2021-02-20 16:03:58 -0500

    finished core gameplay

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,12 @@
 class Game
-  attr_reader :word, :guess, :available_letters, :solved_letters, :incorrect_letters
+  attr_reader :word, :guess, :available_letters, :solved_letters, :incorrect_letters, :guesses
 
   def initialize
     @word = word
     @solved_letters = []
     @available_letters = []
     @incorrect_letters = []
+    @guesses = 7
     puts 'Welcome to a game of Hangman.'
     puts 'You can only have 7 incorrect guesses before you hang the man.'
     puts 'To quit, type "exit" and the game will end.'
@@ -42,11 +43,14 @@ class Game
         @solved_letters.push(guess)
       else
         @incorrect_letters.push(guess) 
+        @guesses -= 1
       end                                         
     else
       puts "Sorry, not sure what you meant to say. Please try again."
       get_guess
     end
+    guesses_left
+    game_over?
     play
   end
 
@@ -59,22 +63,41 @@ class Game
   private
 
   def show_progress
-    @word.split('').each { |n|
-      print '_ '
-    }
+    if @solved_letters == []
+      @word.split('').each { |n|
+          print '_ '
+      }
+    else
+      @word.split('').each { |n|
+        if @solved_letters.include? n
+          print "#{n} "
+        else
+          print '_ '
+        end
+      }
+    end
     puts
     puts "Letters you already tried:"
     p @incorrect_letters
   end
 
-  def show_bad_guesses
-  end
-
   def guesses_left
-
+    puts "You have #{@guesses} left."
   end
 
   def game_over?
+    if @guesses == 0 
+      puts "Sorry, game over!"
+      puts "the word you were trying to guess was '#{@word}'."
+      exit
+    end
+
+    word_array = @word.chars
+
+    if word_array.sort.uniq == @solved_letters.sort
+      puts "Congradulations! You won the game with the word '#{@word}'!"
+      exit
+    end
   end
 end
 
